4f239f0e79d61c2fb319528c4dfca910a6f14b52
Soumya1743 <soumyarangaihagari043@gmail.com>
2024-10-16 15:41:39 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 function App() {
   return (
     <>
+    
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Layout />}>
